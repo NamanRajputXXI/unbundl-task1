c8a8e5c3fb6f620cd313bcfae3a1c054f86ad649
Naman Rajput <namanrajput801@gmail.com>
2023-09-15 22:06:29 +0530

    completed the task

---
+++ script.js
@@ -0,0 +1,39 @@
+const slides = document.querySelectorAll(".slide");
+var counter = 0;
+const totalSlides = slides.length;
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const next = () => {
+  if (counter === 5) {
+    return; // Do nothing if the counter is at the 5th slide
+  }
+
+  counter++;
+  if (counter === totalSlides) {
+    counter = 0; // Loop back to the first slide
+  }
+
+  slideImage();
+};
+
+const prev = () => {
+  if (counter === 0) {
+    return;
+  }
+  counter--;
+  if (counter < 0) {
+    counter = totalSlides - 1;
+  }
+  slideImage();
+};
+
+const slideImage = () => {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+
+setInterval(next, 3000);
